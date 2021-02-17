8cbf8409e43930006613ef406a40c9871591077b
Christina <crissygardner@gmail.com>
2021-02-17 03:51:02 +0000

    in a good stopping place, API and cards are matching, three card spread works :)

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 """Server file for tarot app
 """
-
+import requests
 from flask import Flask, jsonify, render_template
 from model import connect_to_db, Card, Deck, Spread, User, Reading, CardReading
 import crud
@@ -10,6 +10,53 @@ app = Flask(__name__)
 app.secret_key = "dev"
 app.jinja_env.undefined = StrictUndefined
 
+#this is the API request for three random cards
+server_cards3 = requests.get('https://rws-cards-api.herokuapp.com/api/v1/cards/random?n=3')
+#this turns the json into a dictionary
+api_cards = server_cards3.json()
+print(api_cards)
+
+def find_image_name1(api_cards):
+        card_type = api_cards['cards'][0]['type']
+        value = str(api_cards['cards'][0]['value_int'])
+        if len(value) == 1:
+            value = "0" + value
+        if card_type == "major":
+                img_name = "static/cards/m" + value + ".jpg"
+                return img_name
+        elif card_type == "minor":
+                suit = str(api_cards['cards'][0]['suit'])
+                suit_char = suit[0]
+                img_name = "static/cards/" + suit_char + value + ".jpg"
+                return img_name
+
+def find_image_name2(api_cards):
+        card_type = api_cards['cards'][1]['type']
+        value = str(api_cards['cards'][1]['value_int'])
+        if len(value) == 1:
+            value = "0" + value
+        if card_type == "major":
+                img_name = "static/cards/m" + value + ".jpg"
+                return img_name
+        elif card_type == "minor":
+                suit = str(api_cards['cards'][1]['suit'])
+                suit_char = suit[0]
+                img_name = "static/cards/" + suit_char + value + ".jpg"
+                return img_name
+
+def find_image_name3(api_cards):
+        card_type = api_cards['cards'][2]['type']
+        value = str(api_cards['cards'][2]['value_int'])
+        if len(value) == 1:
+            value = "0" + value
+        if card_type == "major":
+                img_name = "static/cards/m" + value + ".jpg"
+                return img_name
+        elif card_type == "minor":
+                suit = str(api_cards['cards'][2]['suit'])
+                suit_char = suit[0]
+                img_name = "static/cards/" + suit_char + value + ".jpg"
+                return img_name
 @app.route('/')
 def homepage():
     """Show the homepage."""
@@ -20,8 +67,6 @@ def homepage():
 def three_card_page():
     """show a three card spread"""
     
-    
-
 @app.route('/api/cards/<int:card_id>')
 def card(card_id):
     """Return a human from the database as JSON."""
@@ -110,23 +155,36 @@ def card_reading(card_reading_id):
 
 @app.route('/cards')
 def get_cards():
-    """View one card."""
-
-    server_cards = crud.get_cards()
-    print(server_cards)
-    three_card_dict = {"card1name":  server_cards[0].card_image,
-                        "card2name":  server_cards[1].card_image,
-                        "card3name":  server_cards[2].card_image
-                        }
-    card1 = three_card_dict.get("card1name")
-    card2 = three_card_dict.get("card2name")
-    card3 = three_card_dict.get("card3name")
-
-    #json_three_card_dict = jsonify(three_card_dict)
-    return render_template('cards.html', card1=card1, card2=card2, card3=card3)
-
+    """View three card."""
+    img_name1 = find_image_name1(api_cards)
+    img_name2 = find_image_name2(api_cards)
+    img_name3 = find_image_name3(api_cards)
+    print(img_name1)
+    print(img_name2)
+    print(img_name3)
+    card1 = api_cards['cards'][0]['name']
+    card1desc = api_cards['cards'][0]['meaning_up']
+    card2 = api_cards['cards'][1]['name']
+    card2desc = api_cards['cards'][1]['meaning_up']
+    card3 = api_cards['cards'][2]['name']
+    card3desc = api_cards['cards'][2]['meaning_up']
         
+    return render_template('cards.html', 
+                            card1=card1, 
+                            card2=card2, 
+                            card3=card3, 
+                            card1desc=card1desc,
+                            card2desc=card2desc,
+                            card3desc=card3desc,
+                            img_name1=img_name1,
+                            img_name2=img_name2,
+                            img_name3=img_name3
+                            )
 
+# @app.route('/threecards')
+    
+        
+    
 if __name__ == '__main__':
     connect_to_db(app)
     app.run(host='0.0.0.0', debug=True)
