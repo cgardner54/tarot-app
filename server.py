0367ac5397725657f3fabe1a295d26256f6984fd
Christina <crissygardner@gmail.com>
2021-02-18 23:49:01 +0000

    randomly selecting which cards to reverse

--- server.py
+++ server.py
@@ -1,6 +1,8 @@
 """Server file for tarot app
 """
-import requests
+import requests, random
+from Imageflip import flip_mirror
+from PIL import Image, ImageOps 
 from flask import Flask, jsonify, render_template
 from model import connect_to_db, Card, Deck, Spread, User, Reading, CardReading
 import crud
@@ -10,53 +12,44 @@ app = Flask(__name__)
 app.secret_key = "dev"
 app.jinja_env.undefined = StrictUndefined
 
-#this is the API request for three random cards
-server_cards3 = requests.get('https://rws-cards-api.herokuapp.com/api/v1/cards/random?n=3')
-#this turns the json into a dictionary
-api_cards = server_cards3.json()
-print(api_cards)
+"""
+Create a class called cards
+here we would have a method to call a card, number it and determine if it should
+be upside down or not
+we should also pull in the title, and meaning
+iif the card is upside down, add the class tag to it, with a value reversed or not.
+"""
+#class Card:
 
-def find_image_name1(api_cards):
-        card_type = api_cards['cards'][0]['type']
-        value = str(api_cards['cards'][0]['value_int'])
+
+def find_image_name(api_cards,card_position):
+        card_type = api_cards['cards'][card_position]['type']
+        value = str(api_cards['cards'][card_position]['value_int'])
+        """this figures out if the card's value integer is one digit or two,
+            if the card type is major or minor, the suit, and then creates the correct
+            naming convention to pull the correct image from the server
+        """
         if len(value) == 1:
             value = "0" + value
         if card_type == "major":
                 img_name = "static/cards/m" + value + ".jpg"
                 return img_name
         elif card_type == "minor":
-                suit = str(api_cards['cards'][0]['suit'])
+                suit = str(api_cards['cards'][card_position]['suit'])
                 suit_char = suit[0]
                 img_name = "static/cards/" + suit_char + value + ".jpg"
                 return img_name
 
-def find_image_name2(api_cards):
-        card_type = api_cards['cards'][1]['type']
-        value = str(api_cards['cards'][1]['value_int'])
-        if len(value) == 1:
-            value = "0" + value
-        if card_type == "major":
-                img_name = "static/cards/m" + value + ".jpg"
-                return img_name
-        elif card_type == "minor":
-                suit = str(api_cards['cards'][1]['suit'])
-                suit_char = suit[0]
-                img_name = "static/cards/" + suit_char + value + ".jpg"
-                return img_name
+def orient_the_card():
+    """This randomly decides what direction the card should appear.
+        Cards have a 1/4 chance of being reversed."""
+    random_number = random.randint(0, 3)
+    if random_number == 0:
+        return "reversed_card"
+    else:
+        return "upright"
 
-def find_image_name3(api_cards):
-        card_type = api_cards['cards'][2]['type']
-        value = str(api_cards['cards'][2]['value_int'])
-        if len(value) == 1:
-            value = "0" + value
-        if card_type == "major":
-                img_name = "static/cards/m" + value + ".jpg"
-                return img_name
-        elif card_type == "minor":
-                suit = str(api_cards['cards'][2]['suit'])
-                suit_char = suit[0]
-                img_name = "static/cards/" + suit_char + value + ".jpg"
-                return img_name
+
 @app.route('/')
 def homepage():
     """Show the homepage."""
@@ -154,21 +147,30 @@ def card_reading(card_reading_id):
                         'message': 'No reading found with that ID'})
 
 @app.route('/cards')
-def get_cards():
+def get_cards(position1=0, position2=1, position3=2):
+    #this is the API request for three random cards
+    server_cards3 = requests.get('https://rws-cards-api.herokuapp.com/api/v1/cards/random?n=3')
+    #this turns the json into a dictionary
+    api_cards = server_cards3.json()
+    print(api_cards)
     """View three card."""
-    img_name1 = find_image_name1(api_cards)
-    img_name2 = find_image_name2(api_cards)
-    img_name3 = find_image_name3(api_cards)
-    print(img_name1)
-    print(img_name2)
-    print(img_name3)
-    card1 = api_cards['cards'][0]['name']
-    card1desc = api_cards['cards'][0]['meaning_up']
-    card2 = api_cards['cards'][1]['name']
-    card2desc = api_cards['cards'][1]['meaning_up']
-    card3 = api_cards['cards'][2]['name']
-    card3desc = api_cards['cards'][2]['meaning_up']
-        
+    img_name1 = find_image_name(api_cards, position1)
+    img_name2 = find_image_name(api_cards, position2)
+
+    img_name3 = find_image_name(api_cards, position3)
+
+
+
+    card1 = api_cards['cards'][position1]['name']
+    card1desc = api_cards['cards'][position1]['meaning_up']
+    card2 = api_cards['cards'][position2]['name']
+    card2desc = api_cards['cards'][position2]['meaning_up']
+    card3 = api_cards['cards'][position3]['name']
+    card3desc = api_cards['cards'][position3]['meaning_up']
+    orient_the_card1 = orient_the_card()
+    orient_the_card2 = orient_the_card()
+    orient_the_card3 = orient_the_card()
+
     return render_template('cards.html', 
                             card1=card1, 
                             card2=card2, 
@@ -178,7 +180,10 @@ def get_cards():
                             card3desc=card3desc,
                             img_name1=img_name1,
                             img_name2=img_name2,
-                            img_name3=img_name3
+                            img_name3=img_name3,
+                            orient_the_card1=orient_the_card1,
+                            orient_the_card2=orient_the_card2,
+                            orient_the_card3=orient_the_card3
                             )
 
 # @app.route('/threecards')
---
+++ stuff.js
@@ -0,0 +1,12 @@
+AJAX: 
+
+onclick handler, target the cards:
+onclick reveal cards
+onhover of card image reveal meaning, upright or reversed
+
+
+object.addEventListener("click", myScript);
+
+object.onclick = function(){myScript};
+
+<element onclick="myScript">
